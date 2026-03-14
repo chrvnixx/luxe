653c661610b1181d6b813997435b3a1312fb0a8b
chrvnixx <emmanueladekola91@gmail.com>
2026-03-14 22:41:48 +0100

    Added email verification controller

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,12 +3,13 @@ import bcrypt from "bcrypt";
 import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
 
 export async function signup(req, res) {
-  const { email, password, name, phone } = req.body;
+  const { email, name, password, phone } = req.body;
+
   try {
     const userAlreadyExists = await User.findOne({ email });
 
     if (userAlreadyExists) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: "User Already Exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -20,15 +21,15 @@ export async function signup(req, res) {
       password: hashedPassword,
       phone: phone,
       verificationToken: verificationToken,
-      verificationTokenExpiresAt: Date.now() + 15 * 60 * 60,
+      verificationTokenExpiresAt: Date.now() + 15 * 60 * 1000,
     });
 
     await user.save();
 
     generateTokenAndSetCookie(res, user._id);
 
-    res.status(200).json({
-      message: "Signup completed successfully",
+    res.status(201).json({
+      message: "User created successfully",
       user: { ...user._doc, password: undefined },
     });
   } catch (error) {
@@ -37,25 +38,33 @@ export async function signup(req, res) {
   }
 }
 
-export async function login(req, res) {
-  const { email, password } = req.body;
+export async function verifyEmail(req, res) {
+  const { code } = req.body;
   try {
-    const user = await User.findOne({ email });
+    if (!code) {
+      return res.status(400).json({ message: "Input verification code" });
+    }
+
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
 
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid or expired Code" });
     }
 
-    const verifyPassword = await bcrypt.compare(password, user.password);
-
-    if (!verifyPassword) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    user.lastLogin = Date.now();
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
 
     await user.save();
 
-    res.status(200).json({ user: { ...user._doc, password: undefined } });
-  } catch (error) {}
+    res
+      .status(200)
+      .json({ message: "User is verified", user: { ...user._doc } });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+    console.log("Error in verify email controller", error);
+  }
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { login, signup } from "../controllers/authControllers.js";
+import { signup, verifyEmail } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/verify-email", verifyEmail);
 
 export default router;
